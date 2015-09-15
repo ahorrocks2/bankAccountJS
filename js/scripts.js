aafe0ead44c171d6d2d18bbeb2a0fa3344aacc75
Alyssa Horrocks & JuliannaGreen <student@epicodus.com>
2015-09-15 16:58:52 -0700

    Allow user to deposit and withdraw from account.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,49 @@
 function Account(name, amount) {
   this.name = name;
-  this.amount = amount;
+  this.amount = parseInt(amount);
 };
 
 Account.prototype.withdrawl = function(withdrawl) {
-  var total = this.amount;
-  return total -= withdrawl;
+  this.amount = this.amount - parseInt(withdrawl);
+  return this.amount;
 };
 
 Account.prototype.deposit = function(deposit) {
-  var total = this.amount;
-  return total += deposit;
+  this.amount = this.amount + parseInt(deposit);
+  return this.amount;
 };
 
 $(document).ready(function() {
   $("form#accountInfo").submit(function(event) {
-    var newName = $("input#name").val()
-    var newDeposit = $("input#initialDeposit").val()
-    var result = new Account(newName, newDeposit);
-      var resultName = result.name;
-      var resultDeposit = result.amount;
+    var newName = $("input#name").val();
+    var newDeposit = $("input#initialDeposit").val();
+    var resultAccount = new Account(newName, newDeposit);
+      var resultName = resultAccount.name;
+      var resultDeposit = resultAccount.amount;
 
-    $(".output").text(resultName + ", " + resultDeposit);
+    $(".output").text(resultName + " " + "$" + resultDeposit);
     $("#result").show();
     event.preventDefault();
+
+    $("form#withdrawl").submit(function(event) {
+      var newWithdrawl = $("input#withdrawl").val();
+      var withdrawlResult = resultAccount.withdrawl(newWithdrawl);
+
+      $(".output").empty().text(resultName + " $" + withdrawlResult);
+
+      $("input").val("");
+      event.preventDefault();
+    });
+
+    $("form#deposit").submit(function(event) {
+      var newDeposit = $("input#deposit").val();
+      var depositResult = resultAccount.deposit(newDeposit);
+
+      $(".output").empty().text(resultName + " $" + depositResult);
+
+      $("input").val("");
+      event.preventDefault();
+    });
+
   });
 });
